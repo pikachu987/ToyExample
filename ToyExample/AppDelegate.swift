63bf297a1ab78affc19fc854e3773d3cd2b5a6f3
pikachu987 <pikachu77769@gmail.com>
2021-06-03 09:44:14 +0900

    Initial commit

---
+++ ToyExample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ToyExample
+//
+//  Created by corpdocfriends on 2021/06/03.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
